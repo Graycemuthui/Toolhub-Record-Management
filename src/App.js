6a172d675c35bb10545c0d8e8853f58a9b21a054
Grayce Muthui <graycemuthui20@gmail.com>
2022-10-24 12:45:01 +0300

    Create the topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { ColorModeContext, useMode } from "./color";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import TopBar from "./components/dashboard/topbar";
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <TopBar />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
